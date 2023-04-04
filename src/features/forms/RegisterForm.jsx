c85cc8262b6946092a8a809f7222dc1e8ec4b547
JaeranKIM <devjaeran@gmail.com>
2023-04-04 16:18:14 +0900

    [feat] 메인으로 돌아가는 링크 임시 생성 #4

--- src/features/forms/RegisterForm.jsx
+++ src/features/forms/RegisterForm.jsx
@@ -4,6 +4,7 @@ import { useFormInput } from "../../hooks/useFormInput";
 import { Input } from "../../components/Input";
 import { RegisterBtn } from "../../components/Buttons";
 import { Flex } from "../../components/Flex";
+import { Link, useNavigate } from "react-router-dom";
 
 function RegisterForm() {
   const [formState, setFormState, handleInputChange] = useFormInput();
@@ -15,7 +16,7 @@ function RegisterForm() {
 
   return (
     <Flex as="form" onSubmit={handleSubmit} fd="column" gap="10">
-      <RegisterBtn>로고 자리(메인으로 돌아감)</RegisterBtn>
+      <Link to="/">로고 자리(메인으로 돌아감)</Link>
       {registerInputList.map((input, index) => (
         <Input
           key={index}
