73aec3ff2579f005c48e965561de376bd3386015
JaeranKIM <devjaeran@gmail.com>
2023-04-06 00:36:11 +0900

    [tempSave] 회원가입 유효성검사 진행중, 이메일 중복확인 진행중 #14

--- src/features/forms/RegisterForm.jsx
+++ src/features/forms/RegisterForm.jsx
@@ -29,14 +29,42 @@ function RegisterForm() {
 
   //회원가입 버튼 클릭시 useRegister에 payload(registerInfo) 전달
   const registerHandler = e => {
-    e.preventDefault();
-    register(registerInfo);
+    //빈 값이 아닐때 register에 payload 보내기
+    if (registerInfo.email === "") {
+      alert("이메일을 입력해주세요.");
+      e.preventDefault();
+    } else if (registerInfo.password === "") {
+      alert("비밀번호를 입력해주세요.");
+      e.preventDefault();
+    } else if (registerInfo.nickname === "") {
+      alert("닉네임을 입력해주세요.");
+      e.preventDefault();
+    } else if (registerInfo.nickname.length < 2) {
+      e.preventDefault();
+      alert("닉네임을 2글자 이상 입력해주세요.");
+    } else {
+      e.preventDefault();
+      register(registerInfo);
+    }
   };
 
   //이메일 중복검사
+  const checkEmail = {
+    email: registerInfo.email,
+  };
+
   const emailConfirmHandler = e => {
-    e.preventDefault();
-    emailConfirm(registerInfo.email);
+    //빈값이 아닐때 보낼 수 있게 하기
+    if (registerInfo.email === "") {
+      alert("이메일을 입력해주세요.");
+      e.preventDefault();
+    } else if (!emailRegExp.test(registerInfo.email)) {
+      e.preventDefault();
+      return <div>이메일 형식이 올바르지 않습니다.</div>;
+    } else {
+      e.preventDefault();
+      emailConfirm(checkEmail);
+    }
   };
 
   //이메일, 비밀번호 형식 정규식 -> 확인해볼것
--- src/hooks/register,login/useEmailConfirm.js
+++ src/hooks/register,login/useEmailConfirm.js
@@ -7,10 +7,15 @@ export function useEmailConfirm() {
       const data = await apis.post("/user/emailconfirm", payload);
       return data;
     },
+    onSuccess: data => {
+      alert("사용 가능한 이메일입니다.");
+    },
+    onError: error => {
+      alert(error.response.data.errorMessage);
+    },
   });
 
   return {
     emailConfirm: mutate,
-    error,
   };
 }
