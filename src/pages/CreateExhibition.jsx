ab8942f14feabe02e6b5f4a2ef1bd782e5bff952
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-04-06 21:52:54 +0900

    [feat] 카카오 주소 라이브러리 사용으로 상세주소 받아오기 #21

--- src/pages/CreateExhibition.jsx
+++ src/pages/CreateExhibition.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Article } from "../shared/GlobalStyled";
+import Header from "../components/Header";
+import { useDaumPostcodePopup } from "react-daum-postcode";
+import styled from "styled-components";
 
 function CreateExhibition() {
-  return <div>CreateExhibition</div>;
+  const [exhibition, setExhibition] = useState({
+    address: "",
+    zonecode: "",
+  });
+
+  const open = useDaumPostcodePopup(process.env.REACT_APP_KAKAO_ADDRESS_URL);
+
+  const handleComplete = (data) => {
+    // let fullAddress = data.address;
+    // let extraAddress = "";
+    // if (data.addressType === "R") {
+    //   if (data.bname !== "") {
+    //     extraAddress += data.bname;
+    //   }
+    //   if (data.buildingName !== "") {
+    //     extraAddress +=
+    //       extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+    //   }
+    //   fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    // }
+    setExhibition((old) => {
+      return { ...old, address: data.address, zonecode: data.zonecode };
+    });
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
+  console.log("exhibition", exhibition);
+  return (
+    <>
+      <Header />
+      <Article>
+        <Box>
+          <h1>작성구역. 카카오 지도 api가지고 오기</h1>
+          <button type="button" onClick={handleClick}>
+            지도
+          </button>
+          <input value={exhibition.address} readonly placeholder="주소" />
+          <input value={exhibition.zonecode} readonly placeholder="우편번호" />
+        </Box>
+      </Article>
+    </>
+  );
 }
 
 export default CreateExhibition;
+
+const Box = styled.div`
+  background-color: #b3f1ae;
+  margin: 50px;
+  padding: 50px;
+`;
