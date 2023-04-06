ffe9619e563f7002142de4b67a053e1cb03619e6
JaeranKIM <devjaeran@gmail.com>
2023-04-06 15:46:30 +0900

    [style] 로그인 페이지 빈 값, 에러메시지 alert 띄우는 기능 구현, 기존에 사용하던 코드 주석처리 해뒀던 것들 삭제함 #24

--- src/features/forms/LoginForm.jsx
+++ src/features/forms/LoginForm.jsx
@@ -50,30 +50,3 @@ function LoginForm() {
 }
 
 export default LoginForm;
-
-//기존 작성본---------------------------------------------------------
-// const [formState, setFormState, handleInputChange] = useFormInput();
-// const handleSubmit = event => {
-//   event.preventDefault();
-//   login(formState);
-//   setFormState({});
-// };
-
-// return (
-//   <Flex as="form" onSubmit={handleSubmit} fd="column" gap="10">
-//     <Link to="/">로고 자리(메인으로 돌아감)</Link>
-//     {loginInputList.map((input, index) => (
-//       <Input
-//         key={index}
-//         label={input.label}
-//         inputProps={{
-//           type: input.type,
-//           name: input.name,
-//           value: formState[input.name] || "",
-//           onChange: handleInputChange,
-//         }}
-//       />
-//     ))}
-//     <LoginBtn type="submit">로그인</LoginBtn>
-//   </Flex>
-// );
