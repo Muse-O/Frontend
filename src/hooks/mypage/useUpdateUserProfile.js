a1dcce4685b50e899533d10a7dba32622f9cc681
JaeranKIM <devjaeran@gmail.com>
2023-04-07 21:45:51 +0900

    [feat] 텍스트 수정 성공, 이미지 수정 남음 #29

--- src/hooks/mypage/useUpdateUserProfile.js
+++ src/hooks/mypage/useUpdateUserProfile.js
@@ -1,19 +1,20 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { apis_token } from "../../api/apis";
+import { apis } from "../../api/apis";
 import { keys } from "../../shared/queryKeys";
 import { cookies } from "../../shared/cookies";
 
 export const useUpdateUserProfile = () => {
-  const queryClient = useQueryClient();
   const token = cookies.get("access_token");
+  const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
     mutationFn: async payload => {
-      await apis_token.patch("/mypage", payload, {
+      const data = await apis.patch("/mypage", payload, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
+      return data;
     },
     onSuccess: () => {
       //성공시 유저프로필 불러오기
