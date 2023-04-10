4cf20f8c30f30d3b1d30801c684c3187e4b58892
JaeranKIM <devjaeran@gmail.com>
2023-04-11 00:47:08 +0900

    [style] 레이아웃 대강 잡아놓음 #29

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import UserProfile from "../features/mypage/UserProfile";
+import Header from "../components/Header";
+import { Article } from "../shared/GlobalStyled";
 
 function MyPage() {
   return (
     <>
-      <UserProfile />
-      {/* 추후 좋아요 누른 전시, 아트그램 컴포넌트 추가될 예정  */}
+      <Header />
+      <Article>
+        <UserProfile />
+        {/* 추후 좋아요 누른 전시, 아트그램 컴포넌트 추가될 예정  */}
+      </Article>
     </>
   );
 }
