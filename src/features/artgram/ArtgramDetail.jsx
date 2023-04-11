89ecca293ac146826eaa6e1ba0c577ed0aa6d98e
19Edwin92 <b_a_s_e@naver.com>
2023-04-11 12:06:41 +0900

    [add] 아트그램 상세페이지 이미지슬라이더 구현 #43

--- src/features/artgram/ArtgramDetail.jsx
+++ src/features/artgram/ArtgramDetail.jsx
@@ -14,7 +14,46 @@ import { usePostingtime } from "../../hooks/artgram/usePostingtime";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "none" }}
+      onClick={onClick}
+    />
+  );
+}
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block flex",
+        background: "#fffffff6",
+        position: "absolute",
+        right: "10px",
+        width: "50px",
+        height: "50px",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "50px",
+        color: "black",
+      }}
+      onClick={onClick}
+    >
+      <div style={{position:"relative", right:"8px"}}>
+        <FaChevronRight size={24} color="#e20303" />
+      </div>
+    </div>
+  );
+}
 
 function ArtgramDetail({pos}) {
   const { allArtgram, modalState, setModalState } = pos;
@@ -26,11 +65,16 @@ function ArtgramDetail({pos}) {
   );
   const [timehandle] = usePostingtime()  
 
+
+
   const settings = {
-    // dots: true,
-    // speed: 500, // 속도조절
+    speed: 500, // 속도조절
+    // infinite:false,
     slidesToShow: 1, // 화면에 보여지는 슬라이더의 수 
     slidesToScroll: 1, // 한번에 넘길 슬라이더이더의 수 
+    style:{width:"100%", position:"relative"},
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
   };
 
   if (isLoading || isError) {
@@ -47,7 +91,13 @@ function ArtgramDetail({pos}) {
         <Flex>
           {/* 상세모달페이지 (1) 이미지 삽입공간 --------------------------------------------------------- */}
           <Modal.ModalinnerImgDiv width="70%">
-            <Modal.ModdalinnerImg src={allArtgram.ArtgramImgs[0].imgUrl} />
+            {/* <Modal.ModdalinnerImg src={allArtgram.ArtgramImgs[0].imgUrl} /> */}
+            <Slider {...settings}>
+              {allArtgram.ArtgramImgs.map(img => (
+                <Modal.ModdalinnerImg src={img.imgUrl} width="100"/>
+              ))}
+            </Slider>
+
           </Modal.ModalinnerImgDiv>
           {/* 상세모달페이지 (2) 텍스트 공간 ------------------------------------------------------------ */}
           <Modal.ModalinnerDiv width="30%">
