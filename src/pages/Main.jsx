79a8bfa0fa7d67a21e75bb48623a9291388299d7
19Edwin92 <b_a_s_e@naver.com>
2023-04-14 12:15:07 +0900

    [tempSave] 메인페이지 레이아웃(2) #64

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState, useEffect, useRef }  from "react";
 import Header from "../components/Header";
 import { Article } from "../shared/GlobalStyled";
 import styled from "styled-components";
 import { Flex } from "../components/Flex";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <CustomPrevButton
+      onClick={onClick}>
+      <Icons children={<FaChevronLeft/>}/>
+    </CustomPrevButton>
+  );
+}
+
+
+function SampleNextArrow(props) {
+  const { onClick } = props;
+  return (
+    <CustomNextButton
+      onClick={onClick}>
+      <Icons children={<FaChevronRight/>}/>
+    </CustomNextButton>
+  );
+}
 
 function Main() {
+  // Slider
+  const [mainSlider, setMainSlider] = useState(null);
+  const [subSlider, setSudSilder] = useState(null);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const mainSliderRef = useRef(null);
+  const subSliderRef = useRef(null);
+
+  useEffect(() => {
+    setMainSlider(mainSliderRef.current);
+    setSudSilder(subSliderRef.current);
+  }, []);
+
+  const mainSlidersettings = {
+    asNavFor: subSlider,
+    ref: slider => (mainSliderRef.current = slider),
+    style:{position:"relative"}
+  };
+
+  const Indexhandler = (oldIndex, newIndex) => {
+    setCurrentSlideIndex(newIndex+1)
+  }
+
+  const subSlidersettings = {
+    className:"slider2",
+    asNavFor: mainSlider,
+    ref: slider => (subSliderRef.current = slider),
+    slidesToShow: 4, 
+    swipeToSlide: true,
+    focusOnSelect: true,
+    autoplay: true, 
+    autoplaySpeed: 2000,
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
+    style:{position:"static", backgroundColor:"lightyellow"},
+    beforeChange: Indexhandler
+  };
+
   return (
     <>
       <Header />
@@ -14,32 +76,140 @@ function Main() {
             <MainH4 children="전시 종류" />
           </FirstDiv>
           <SecondDiv height="570">
-            <MainH1 children="최신 전시 - 슬라이더 필요"/>
+            <MainH1 children="최신 전시 - 슬라이더 필요" />
             <SecondWrap ai="center" gap="23">
-             {Array(6).fill(null).map(el => (
-              <SecondInner>
-                <SecondImg/>
-                <p className="firstP">제목</p>
-                <p className="secondP">2023.04.14-2023.04.20</p>
-                <p>위치</p>
-              </SecondInner>
-             ))}
+              {Array(6)
+                .fill(null)
+                .map((el,index) => (
+                  <SecondInner key={index}>
+                    <SecondImg />
+                    <p className="firstP">제목</p>
+                    <p className="secondP">2023.04.14-2023.04.20</p>
+                    <p className="thirdP">위치</p>
+                  </SecondInner>
+                ))}
             </SecondWrap>
-            <div style={{minWidth:"40px", minHeight:"40px", backgroundColor:"lightgray", borderRadius:"50px", position:"absolute", top:"10px", right:"0"}}></div>
-            <div style={{minWidth:"40px", minHeight:"40px", backgroundColor:"lightgray", borderRadius:"50px", position:"absolute", top:"10px", right:"64px"}}></div>
+            <div
+              style={{
+                minWidth: "40px",
+                minHeight: "40px",
+                backgroundColor: "lightgray",
+                borderRadius: "50px",
+                position: "absolute",
+                top: "10px",
+                right: "0",
+              }}
+            ></div>
+            <div
+              style={{
+                minWidth: "40px",
+                minHeight: "40px",
+                backgroundColor: "lightgray",
+                borderRadius: "50px",
+                position: "absolute",
+                top: "10px",
+                right: "64px",
+              }}
+            ></div>
           </SecondDiv>
-          <ThirdDiv height="890">
-            <MainH1 children="TOP 10"/>
+          <ThirdDiv height="790">
+            <MainH1 children="TOP 10" />
             <ThirdWrap gap="23" fw="wrap">
-            {Array(10).fill(null).map(el => (<ThirdInner/>))}
-             
+              {Array(10)
+                .fill(null)
+                .map((el, index) => (
+                  <ThirdInner key={index}>
+                    <div className="number">{index + 1}</div>
+                    <div className="exhibition"></div>
+                    <div className="innerText">
+                      <p className="firstP">대지의 시간 The Time of Earth</p>
+                      <p className="secondP">2023.04.14-2023.04.20</p>
+                    </div>
+                  </ThirdInner>
+                ))}
             </ThirdWrap>
           </ThirdDiv>
           <FourthDiv height="655">
-            <MainH1 children="예정 전시"/>
+            <MainH1 children="예정 전시" />
+            <FourthWrap>
+              <div className="exhibitionimg"></div>
+              <div className="exhibitioninfo">
+                {Array(4)
+                  .fill(null)
+                  .map((el,index) => (
+                    <Exhibitioninfo key={index}>
+                      <div className="date">
+                        <p>30</p>
+                        <p>Aug</p>
+                      </div>
+                      <div className="exhibitininfo">
+                        <p>대지의 시간</p>
+                        <p>The Time of Earth</p>
+                      </div>
+                      <div className="exhibitionlocation">
+                        <p>서울 : 마이아트뮤지엄</p>
+                      </div>
+                    </Exhibitioninfo>
+                  ))}
+              </div>
+            </FourthWrap>
           </FourthDiv>
           <FifithDiv height="800">
-          <MainH1 children="아트그램"/>
+            <MainH1 children="아트그램" />
+            <FifitInner>
+              <SliderDiv>
+                <Slider {...mainSlidersettings}>
+                 {Array(6).fill(null).map((el,index) => (
+                   <MainSliderWrap key={index}>
+                   <div className="mainsliderimg">
+                    <img alt={`이미지${index+1}`} width="100"/>
+                   </div>
+                   <div className="profile">
+                    <div className="profileimg"></div>
+                    <div className="profileNickname"><p><span>by</span> userNickname {index+1}</p></div>
+                   </div>
+                 </MainSliderWrap>
+                 ))}
+                </Slider>
+                <div style={{backgroundColor:"lightcyan", padding:"50px"}}>
+                <Slider {...subSlidersettings}>
+                  <SubSliderWrap>
+                    <h3>2</h3>
+                  </SubSliderWrap>
+                  <SubSliderWrap>
+                    <h3>3</h3>
+                  </SubSliderWrap>
+                  <SubSliderWrap>
+                    <h3>4</h3>
+                  </SubSliderWrap>
+                  <SubSliderWrap>
+                    <h3>5</h3>
+                  </SubSliderWrap>
+                  <SubSliderWrap>
+                    <h3>6</h3>
+                  </SubSliderWrap>
+                  <SubSliderWrap>
+                    <h3>1</h3>
+                  </SubSliderWrap>
+                </Slider>
+                </div>
+                
+                {/* <div
+                  style={{
+                    position: "absolute",
+                    backgroundColor: "lightgray",
+                    borderRadius: "20px",
+                    width: "50px",
+                    height: "20px",
+                    textAlign: "center",
+                    top: "60px",
+                    right: "15px",
+                  }}
+                >
+                  {currentSlideIndex}/6
+                </div> */}
+              </SliderDiv>
+            </FifitInner>
           </FifithDiv>
         </MainWrap>
       </Article>
@@ -55,6 +225,8 @@ const MainWrap = styled.div`
   flex-direction: column;
   /* background-color: yellow; */
   margin: 65px 75px;
+  color: #242424;
+  font-family: 'SpoqaHanSansNeo-Regular';
 `;
 
 const FirstDiv = styled.div`
@@ -75,6 +247,8 @@ const SecondDiv = styled.div`
     const SecondWrap = styled(Flex)`
       /* background-color: white; */
       margin-top: 36px;
+      max-width: 1525px;
+      overflow: hidden;
     `
 
     const SecondInner = styled.div`
@@ -84,18 +258,17 @@ const SecondDiv = styled.div`
       max-height: 390px;
       background-color: lightgray;
 
-      p {
-        font-family: 'SpoqaHanSansNeo-Regular';
-        font-size: 16px;
-      }
-
       .firstP {
         margin-top: 24px;
         font-size: 20px;
       }
       .secondP{
         margin-top: 4px;
+        font-size: 16px;
         color: #5A5A5A;
+      }
+      .thirdP{
+        margin-top: 4px;
       }
     `
 
@@ -111,12 +284,13 @@ const SecondDiv = styled.div`
 const ThirdDiv = styled.div`
   width: 100%;
   /* background-color: #00eaff96;; */
-  max-height: ${pos=>pos.height}px;
+  max-height: fit-content;
   min-height: ${pos=>pos.height}px;
 `
     const ThirdWrap = styled(Flex)`
-    /* background-color: white; */
     margin-top: 36px;
+    max-width: 1525px;
+    padding-bottom: 24px;
     `
 
 const ThirdInner = styled.div`
@@ -125,16 +299,109 @@ const ThirdInner = styled.div`
   min-height: 148px;
   max-height: 148px;
   margin-bottom: -23px;
+  padding: 12px 0;
   border-bottom: 1px solid #D9D9D9;
   background-color: #f5f5f59a;
+  display: grid;
+  grid-template-columns: 100px 88px 1fr;
+
+  .number{
+    font-size: 20px;
+  }
+  .exhibition {
+    min-width: 87px;
+    max-width: 87px;
+    min-height: 124px;
+    max-height: 124px;
+    background-color: lightgray;
+  }
+
+  .innerText {
+    margin-left: 152px;
+    padding: 47px 0;;
+
+      .firstP {
+        font-size: 20px;
+        margin-bottom: 4px;
+      }
+      .secondP{
+        color: #5A5A5A;
+        font-size: 12px;
+      }
+  }
 `
 
-
 const FourthDiv = styled.div`
+  position: relative;
   width: 100%;
-  background-color: #8c00ff95;;
+  margin-top: 100px;
+  /* background-color: #8c00ff95;; */
   max-height: ${pos=>pos.height}px;
   min-height: ${pos=>pos.height}px;
+`
+
+const FourthWrap = styled.div`
+  position: relative;
+  min-height: 480px;
+  max-height: 480px;
+  margin-top:36px;
+  /* background-color: #f5f5f59a; */
+  display: grid;
+  grid-template-columns: 387px 1fr;
+  gap: 80px;
+
+  .exhibitionimg {
+    min-width: 387px;
+    max-width: 387px;
+    min-height: 480px;
+    max-height: 480px;
+    background-color: #292929;
+  }
+  .exhibitioninfo{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap:24px;
+  }
+  `
+
+const Exhibitioninfo = styled.div`
+  height: 102px;
+  display: grid;
+  grid-template-columns: 82px minmax(200px, 1fr) 250px;
+  gap: 112px;
+  border: 1px solid black;
+  border-radius:8px;
+  padding: 25px;
+  &:hover {
+    background-color: #232323;
+    color: #E6E6E6;
+  }
+  .date {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+  }
+
+  .exhibitininfo {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    font-size: 16px;
+    p:first-child {
+      font-size: 20px;
+    }
+  }
+
+  .exhibitionlocation {
+    text-align: end;
+    padding-right: 57px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `
 
 const FifithDiv = styled.div`
@@ -143,6 +410,14 @@ const FifithDiv = styled.div`
   max-height: ${pos=>pos.height}px;
   min-height: ${pos=>pos.height}px;
 `
+const FifitInner = styled.div`
+  background-color: #E6E6E6;
+  margin-top: 36px;
+  min-height: 533px;
+  max-height: 533px;
+  position: relative;
+`
+
 
 const MainH1 = styled.h4`
   font-family: 'S-CoreDream-3Light';
@@ -153,3 +428,108 @@ const MainH4 = styled.h4`
   font-family: 'S-CoreDream-3Light';
   font-size: 24px;
 `
+
+const SliderDiv = styled.div`
+  position:relative;
+  display: grid;
+  grid-template-columns: 364px 1fr;
+  height:auto;
+
+  .slider2 {
+    position:absolute;
+    top:90px;
+    right:0;
+    width: 450px;
+    height: 100px;
+  }
+`
+
+const MainSliderWrap = styled.div`
+  box-sizing:border-box;
+  min-height:500px;
+  max-width: 364px;
+  padding: 16px;
+  padding-bottom: 0;
+  background-color: white;
+
+  .mainsliderimg {
+    background-color: skyblue;
+    min-height: 374px;
+    max-height: 374px;
+    margin-bottom: 73px;
+  }
+
+  .profile {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .profileimg {
+    display: inline-block;
+    width: 29px;
+    height: 29px;
+    background-color:lightgray;
+    border-radius: 50px;
+  }
+
+  .profileNickname {
+    display: inline-block;
+    height: 29px;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+
+    span {
+      color:#8C8C8C;
+    }
+  }
+`
+const SubSliderWrap = styled.div`
+  box-sizing:border-box;
+  height:150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h3 {
+    display:block;
+    width:80px;
+    height:100px;
+    line-height:100px;
+    margin:0 auto;
+    background-color:lightgreen;
+    text-align:center;
+    color:white;
+  }
+`
+
+const CustomPrevButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    left: 464px;
+    bottom: 10px;
+    width: 50px;
+    height: 50px;
+    transform: rotateY(-50%);
+    background-color:rgba(80, 80, 80, 0.2);
+    border-radius: 50px;
+    color: black;
+    z-index: 10;
+`;
+
+const CustomNextButton = styled(CustomPrevButton)`
+      left: 564px;
+`
+
+const Icons = styled.div`
+  position: absolute;
+  top: 53%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2.8rem;
+  color: red;
+  text-align: center;
+`;
