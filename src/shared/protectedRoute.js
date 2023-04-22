29ac04a2b3206a8aba4ba89e6f9ab82199d12b66
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-04-22 21:01:27 +0900

    [fix] 프로택티드 라우터 실행 안되는것 수정 #103

--- src/shared/protectedRoute.js
+++ src/shared/protectedRoute.js
@@ -1,10 +1,21 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { usetoken } from "./cookies";
 
-export const ProtectedRoute = ({ isAuthenticated, children }) => {
-  if (!isAuthenticated) {
-    alert("로그인이 필요한 서비스 입니다.");
-    return <Navigate to="/login" />;
-  }
-
+export const ProtectedRoute = ({ isPublic, isLogin, children }) => {
+  const navigator = useNavigate();
+  const { access_token } = usetoken();
+  const isAuthenticated = isPublic || access_token;
+  const isLogined = isLogin || !access_token;
+  //토큰이 있다
+  useEffect(() => {
+    if (!isAuthenticated) {
+      alert("로그인이 필요한 서비스 입니다.");
+      navigator("/login");
+    }
+    if (!isLogined) {
+      navigator("/");
+    }
+  }, [isAuthenticated, isLogined]);
   return children;
 };
