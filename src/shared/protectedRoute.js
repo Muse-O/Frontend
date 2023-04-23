5277c95785d48ccc2f138dcc505b7890ac15c5fb
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-04-23 22:49:31 +0900

    [update] 프로택티드 라우터 설명 추가 #103

--- src/shared/protectedRoute.js
+++ src/shared/protectedRoute.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { usetoken } from "./cookies";
 
 export const ProtectedRoute = ({ isPublic, isLogin, children }) => {
@@ -7,14 +7,17 @@ export const ProtectedRoute = ({ isPublic, isLogin, children }) => {
   const { access_token } = usetoken();
   const isAuthenticated = isPublic || access_token;
   const isLogined = isLogin || !access_token;
-  //토큰이 있다
+  //isAuthenticated 로그인 해야하 가능한 서비스 false는 못 들어감.
+  //isLogined 로그인 했지만 들어가면 안되는곳 false는 못 들어감.
   useEffect(() => {
     if (!isAuthenticated) {
       alert("로그인이 필요한 서비스 입니다.");
       navigator("/login");
+      return;
     }
     if (!isLogined) {
       navigator("/");
+      return;
     }
   }, [isAuthenticated, isLogined]);
   return children;
