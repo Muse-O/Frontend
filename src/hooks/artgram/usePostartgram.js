374469e36e3d6498e964080e43c0417682cd9041
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-05-03 03:02:09 +0900

    [fix] 콘솔 삭제  #148

--- src/hooks/artgram/usePostartgram.js
+++ src/hooks/artgram/usePostartgram.js
@@ -1,15 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apis } from "../../api/apis";
-import { cookies } from '../../shared/cookies'
-import { keys } from '../../shared/queryKeys'
-
+import { cookies } from "../../shared/cookies";
+import { keys } from "../../shared/queryKeys";
 
 const postArtgram = async (payload) => {
-  console.log(payload);
-  const token = cookies.get('access_token')
+  // console.log(payload);
+  const token = cookies.get("access_token");
   const response = await apis.post("/artgram", payload, {
     headers: {
-      Authorization : `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
   });
   return response;
@@ -17,7 +16,7 @@ const postArtgram = async (payload) => {
 
 export const usePostartgram = () => {
   const queryClient = useQueryClient();
-  const  { mutate : postArtgrams } = useMutation(postArtgram, {
+  const { mutate: postArtgrams } = useMutation(postArtgram, {
     onSuccess: (response) => {
       alert(response.data.message);
       queryClient.invalidateQueries(keys.GET_ARTGRAM);
@@ -26,5 +25,5 @@ export const usePostartgram = () => {
       alert("글을 저장하지 못했습니다.", err);
     },
   });
-  return [postArtgrams]
+  return [postArtgrams];
 };
--- src/hooks/search/useSearchRecent.js
+++ src/hooks/search/useSearchRecent.js
@@ -1,30 +1,30 @@
-import { useQuery } from "@tanstack/react-query"
-import { keys } from "../../shared/queryKeys"
-import { apis } from "../../api/apis"
-import { cookies } from "../../shared/cookies"
+import { useQuery } from "@tanstack/react-query";
+import { keys } from "../../shared/queryKeys";
+import { apis } from "../../api/apis";
+import { cookies } from "../../shared/cookies";
 
 export const useSearchRecent = (searchWindow) => {
-  const token = cookies.get("access_token")
-  const {isLoading, isError, data} = useQuery({
-    queryKey : keys.GET_UNIFIEDSEARCHRECENT,
+  const token = cookies.get("access_token");
+  const { isLoading, isError, data } = useQuery({
+    queryKey: keys.GET_UNIFIEDSEARCHRECENT,
     queryFn: async () => {
       const response = await apis.get("/search/recent", {
-        headers : {
-          Authorization : `Bearer ${token}`
-        }
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
-      console.log("recent", response.data.recentHistory);
-      return response.data.recentHistory
+      // console.log("recent", response.data.recentHistory);
+      return response.data.recentHistory;
     },
     enabled: searchWindow,
-    retry :1,
+    retry: 1,
     refetchOnWindowFocus: false,
     onSuccess: () => {
       console.log("최근검색결과 조회");
     },
     onError: (e) => {
       console.log("error", e.message);
-    }
-  })
-  return {isLoading, isError, data} 
-}
+    },
+  });
+  return { isLoading, isError, data };
+};
