876e9ce2e9587fb25c33f0a2d8aa85a863834366
19Edwin92 <b_a_s_e@naver.com>
2023-05-03 02:42:19 +0900

    [fixed] 전시등록에서 발생되는 헤더 상태변화 이슈 #dev

--- src/pages/CreateExhibition.jsx
+++ src/pages/CreateExhibition.jsx
@@ -3,13 +3,13 @@ import { Article } from "../shared/GlobalStyled";
 import Header from "../components/Header";
 import { usePostExhibition } from "../hooks/exhibition/usetPostExhibition";
 import ExhibitionForm from "../features/exhibition/exhibitionCreate/ExhibitionForm";
-import { useRecoilState } from "recoil";
-import { headerStatedefalut } from "../components/headerStore";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { headerStateSearch, headerStatedefalut } from "../components/headerStore";
 
 function CreateExhibition() {
   const [createExhibition] = usePostExhibition();
   const [, setHeaderState] = useRecoilState(headerStatedefalut);
-  const headerState = useAsyncValue(headerStateSearch)
+  const headerState = useRecoilValue(headerStateSearch)
   useEffect(() => {
     setHeaderState({ ...headerState, home: true });
   }, []);
