bca75415a06077adba42a601c5be977b2c0182e5
JaeranKIM <devjaeran@gmail.com>
2023-05-02 16:30:41 +0900

    [style] 알람 1440px 미디어쿼리 적용 #137

--- src/features/mypage/css/AlarmContainerStyle.js
+++ src/features/mypage/css/AlarmContainerStyle.js
@@ -5,12 +5,22 @@ const StAlramContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
+
+  @media (max-width: 1440px) {
+    width: 294px;
+    gap: 3px;
+  }
 `;
 
 const StAlramTitle = styled.div`
   display: flex;
   margin-bottom: 24px;
   gap: 4px;
+
+  @media (max-width: 1440px) {
+    margin-bottom: 18px;
+    gap: 3px;
+  }
 `;
 
 const AlramTitle = styled.div`
@@ -18,15 +28,29 @@ const AlramTitle = styled.div`
   font-size: 24px;
   font-weight: bold;
   text-align: center;
+
+  @media (max-width: 1440px) {
+    font-size: 18px;
+  }
 `;
 
 const StBell = styled.div`
   width: 23px;
   height: 23px;
 
+  @media (max-width: 1440px) {
+    width: 17.25px;
+    height: 17.25px;
+  }
+
   img {
     width: 23px;
     height: 23px;
+
+    @media (max-width: 1440px) {
+      width: 17.25px;
+      height: 17.25px;
+    }
   }
 `;
 
@@ -43,6 +67,11 @@ const StAlramBox = styled.div`
   ::-webkit-scrollbar {
     display: none; /* 크롬, 사파리, 오페라, 엣지 */
   }
+
+  @media (max-width: 1440px) {
+    width: 294px;
+    height: 294px;
+  }
 `;
 
 const StAlramWrap = styled.div`
@@ -51,6 +80,13 @@ const StAlramWrap = styled.div`
   padding: 10px;
   margin-bottom: 1px;
   cursor: pointer;
+
+  @media (max-width: 1440px) {
+    width: 294px;
+    height: 49.5px;
+    padding: 7.5px;
+    margin-bottom: 1px;
+  }
 
   div {
     display: flex;
@@ -68,9 +104,20 @@ const StIconWrap = styled.div`
   align-items: center;
   margin-right: 13px;
 
+  @media (max-width: 1440px) {
+    width: 31.5px;
+    height: 31.5px;
+    margin-right: 9.75px;
+  }
+
   img {
     width: 22px;
     height: 22px;
+
+    @media (max-width: 1440px) {
+      width: 16.5px;
+      height: 16.5px;
+    }
   }
 `;
 
@@ -84,6 +131,11 @@ const StAlramTheme = styled.div`
   font-size: 14px;
   font-weight: 500;
   margin-bottom: 7px;
+
+  @media (max-width: 1440px) {
+    font-size: 10.5px;
+    margin-bottom: 5.25px;
+  }
 `;
 
 const StAlramContent = styled.div`
@@ -92,6 +144,10 @@ const StAlramContent = styled.div`
   font-weight: 400;
   color: #3c3c3c;
   letter-spacing: -0.003em;
+
+  @media (max-width: 1440px) {
+    font-size: 9px;
+  }
 `;
 
 export {
