d81c1330ab4659682cf9336e6c4adbd731a3d12d
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-05-04 19:35:41 +0900

    [fix]  input 중복 확인 유효성검사+공백일때 검색x+이전값과 같다면 검색x #167

--- src/features/exhibition/exhibitoinList/LIst/HeaderSearch.jsx
+++ src/features/exhibition/exhibitoinList/LIst/HeaderSearch.jsx
@@ -5,29 +5,45 @@ import * as EXSearch from "../css/exhibitionHeaderCss/EXSearchCss";
 export const HeaderSearch = () => {
   const [applyTags, setApplyTags] = useRecoilState(EXApplyTagsStore);
   const [Search, setSearch] = useState("");
+
   const onChangeSearch = (e) => {
     const value = e.target.value;
     setSearch(value);
   };
+
+  const checkNoGap = /^\s*$/; //공백으로만 이루어져있는지 확인하는것
+  const checkDuplicateGap = (checkValue) => {
+    return checkValue.trim().replace(/\s+/g, " "); // 입력값을 좌우 공백을 없에고 중복된 공백을 하나의 공백으로 만들어준다.
+  };
+  const checkSearchValue =
+    !checkNoGap.test(Search) && //공백"  "으로만 이루어지지 않아야하고
+    Search !== "" && //null값이면 안되고
+    applyTags.Search !== checkDuplicateGap(Search); // 이전 입력같이랑 입력된 value값의 앞뒤 공백을 자르고 중복된 공백을 삭제한 상태에서 같으면 안되고
+
   const onSearchHandler = () => {
-    setApplyTags((pre) => {
-      return {
-        ...pre,
-        Search: Search,
-      };
+    if (checkSearchValue) {
+      setApplyTags((pre) => {
+        return {
+          ...pre,
+          Search: Search,
+        };
+      });
+    }
+    setSearch((pre) => {
+      return checkDuplicateGap(pre);
     });
-    setSearch("");
   };
   const onKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && checkSearchValue) {
       onSearchHandler();
     }
   };
+
   return (
     <>
       <EXSearch.FilterInputWrap>
         <EXSearch.FilterSearch
-          placeholder={applyTags.Search || "제목 검색"}
+          placeholder={"제목 검색"}
           value={Search}
           onChange={onChangeSearch}
           onKeyDown={onKeyDown}
