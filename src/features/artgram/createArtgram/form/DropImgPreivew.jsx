49223aecb7d201d68e8b02809e68f54a60b6db36
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-05-05 01:31:09 +0900

    [delete] 콘솔 삭재   #167

--- src/features/artgram/createArtgram/form/DropImgPreivew.jsx
+++ src/features/artgram/createArtgram/form/DropImgPreivew.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import * as ArtgramFormparts from "../ArtgramFormImgparts";
 
-function DropImgPreivew({files, setFiles}) {
+function DropImgPreivew({ files, setFiles }) {
   const deletFilesimg = (e, name) => {
-    e.stopPropagation()
-    console.log(name);
-    const find = files.findIndex(names => names.name === name)
-    const filter = [...files.slice(0,find), ...files.slice(find+1)]
-    setFiles(filter)
-  }
+    e.stopPropagation();
+    const find = files.findIndex((names) => names.name === name);
+    const filter = [...files.slice(0, find), ...files.slice(find + 1)];
+    setFiles(filter);
+  };
   useEffect(() => {
     // 마운트 해제시, 데이터 url 취소
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
@@ -28,7 +27,10 @@ function DropImgPreivew({files, setFiles}) {
                   URL.revokeObjectURL(file.preview);
                 }}
               />
-              <ArtgramFormparts.PreviewBoxDelete onClick={(e)=> deletFilesimg(e, file.name)} children="삭제"/>
+              <ArtgramFormparts.PreviewBoxDelete
+                onClick={(e) => deletFilesimg(e, file.name)}
+                children="삭제"
+              />
             </ArtgramFormparts.PreviewBox>
           ))}
       </ArtgramFormparts.DropZonePreview>
